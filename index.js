5f9e4c287ee296927b44fd78db8893312dc71b4f
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-29 19:06:44 -0400

    working on completing

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const port = 2000
 
 server.use(express.json())
 server.use("/api", router )
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "keep it secret!",
+    store: new knexSessionStore({
+        knex: db,
+        creatable: true
+    }),
+}))
 
 server.listen(port, () => {
     console.log( `Running at http://localhost:${port} `)
