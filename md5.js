da93e58870598c6954a8a810ceb34e2d7e16db14
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-28 20:25:03 -0400

    setting up

---
+++ md5.js
@@ -0,0 +1,19 @@
+const { getRounds } = require("bcryptjs")
+const crypto = require("crypto")
+
+const rounts = 100000
+
+const value = "hello world"
+
+console.time("hash time")
+
+let hash = value
+for (let i=0; i < rounds; i++){
+    hash = crypto
+        .createHash("md5")
+        .update(hash)
+        .digest("hex")
+}
+
+console.timeEnd("hash time")
+console.log(`result of ${rounds} MD5 hashes: ${hash}`)
